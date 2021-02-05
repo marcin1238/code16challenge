248c173468a7b92fbd93c187ffedba0293be5563
Marcin Kabza <marcinkabza1238@gmail.com>
2021-02-05 12:36:24 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Net.Http;
+
+class Program
+{
+    static async System.Threading.Tasks.Task Main()
+    {
+        var response = await new HttpClient().GetAsync("https://www.siepomaga.pl/maja");
+        var exchangeRateResponse = await new HttpClient().GetAsync("https://api.exchangeratesapi.io/latest?base=USD");
+        var exchangeRate = Convert.ToDecimal(Regex.Match(await exchangeRateResponse.Content.ReadAsStringAsync(), "(?<=PLN\":)\\d+(\\.\\d+)").Value, new CultureInfo("en-US"));
+        var amount = Convert.ToDecimal(Regex.Match(await response.Content.ReadAsStringAsync(), "(?<=amount-left\' data-value=\')\\d+(\\.\\d+)").Value, new CultureInfo("en-US"));
+        Console.WriteLine($"Maja potrzebuje jeszcze {amount.ToString("C", new CultureInfo("pl-PL"))} lub {(amount / exchangeRate).ToString("C", new CultureInfo("en-US"))}");
+    }
+}
---
+++ README.md
@@ -0,0 +1,32 @@
+# code16challenge
+
+Rules: www.code16challenge.pl
+URL: www.siepomaga.pl/code16challenge
+
+### Prerequisites
+You need to install the software:
+* .NET 5.0
+
+### Installing
+To build: 
+
+```
+dotnet build
+```
+
+To run: 
+
+```
+dotnet run
+```
+
+## Built With
+
+* [.Net](https://www.microsoft.com/net) - The .net framework used
+
+## Versioning
+* Version v0.1
+
+## Authors
+
+* [Marcin Kabza](https://www.linkedin.com/in/marcin-kabza-588ab310b/)
